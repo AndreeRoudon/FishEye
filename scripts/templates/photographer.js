3a769527fe5589b20cb7bc234b305e9b7cdcf52f
Andree Roudon <andree_2000@hotmail.fr>
2023-07-07 12:17:10 +0200

    recuperation des data + affichage index

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,34 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait, country, city, tagline, price } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement('article');
+
+        const link = document.createElement('a');
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        link.setAttribute('href', './photographer.html');
+        link.appendChild(img);
+
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
+
+        const localisation = document.createElement('h3');
+        localisation.textContent = city + ', ' + country;
+
+        const paragraph = document.createElement('p');
+        paragraph.textContent = tagline;
+
+        const Price = document.createElement('span');
+        Price.textContent = price + '€/jour';
+
+        article.appendChild(link);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(localisation);
+        article.appendChild(paragraph);
+        article.appendChild(Price);
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, country, city, tagline, price, getUserCardDOM }
 }
